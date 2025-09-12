2171d25ac265741ad697120715fdf79a5290acf5
zororai <mazaruracarlos@gmail.com>
2025-09-12 14:17:26 +0200

    chore: update MongoDB connection settings from Azure Cosmos to local development

--- src/main/java/com/commstack/coapp/DemoApplication.java
+++ src/main/java/com/commstack/coapp/DemoApplication.java
@@ -10,6 +10,9 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "CO-APP API", version = "1.0", description = "API Information"), servers = @Server(url = "https://coappapi.commapp.online/"))
@@ -20,4 +23,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://coappui.commapp.online") // adjust
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
